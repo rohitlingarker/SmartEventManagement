81dd5cf06e881525c1621b2edf095e07cc432441
rohitlingarker <rohitlingerker@gmail.com>
2025-09-15 12:38:02 +0530

    deploymennt changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import app from "./app.js";
 
 const PORT = process.env.PORT || 3000;
+app.set('trust proxy', 1);
 
-app.listen(PORT, () => {
+app.listen(PORT, '127.0.0.1', () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
